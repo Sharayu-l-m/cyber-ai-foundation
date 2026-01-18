cb2aa8f1783917783fe766783063468abcd2a0ec
Sharayu-l-m <nansha513@gmail.com>
2026-01-17 21:45:19 -0800

    Day5 : python pip

---
+++ pip.py
@@ -0,0 +1,17 @@
+"""python pip practice"""
+
+1.pyjokes
+#pyjokes demo
+
+import pyjokes
+print("joke of the day:")
+print(pyjokes.get_joke())
+
+2.pyttsx3 
+#pyttsx3 demo
+
+import pyttsx3
+engine = pytttsx3.init()
+engine.say(" I am learning python pip libraries ")
+engine.runAndWait()
+
