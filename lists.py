3b80e261c61d882065253d9da4f984c14fae4e50
Sharayu-l-m <nansha513@gmail.com>
2026-01-30 08:37:27 -0800

    Day12.lists

---
+++ lists.py
@@ -0,0 +1,20 @@
+# Creating a list
+fruits = ["apple", "banana", "mango"]
+
+print("Initial list:", fruits)
+
+# Accessing elements
+print("First fruit:", fruits[0])
+
+# Adding elements
+fruits.append("orange")
+print("After adding orange:", fruits)
+
+# Removing elements
+fruits.remove("banana")
+print("After removing banana:", fruits)
+
+# Looping through list
+print("Fruits list:")
+for fruit in fruits:
+    print(fruit)
